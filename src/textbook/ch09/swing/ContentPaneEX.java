712aa8e7276318577bf381640d20b33258c3838d
SMJin <sstaw@naver.com>
2021-12-08 00:05:00 +0900

    first commit

---
+++ src/textbook/ch09/swing/ContentPaneEX.java
@@ -0,0 +1,26 @@
+package textbook.ch09.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPaneEX extends JFrame {
+    public ContentPaneEX() {
+        setTitle("ContentPane과 JFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setBackground(Color.ORANGE);
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("OK"));
+        contentPane.add(new JButton("Cancel"));
+        contentPane.add(new JButton("Ignore"));
+
+        setSize(300, 150);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ContentPaneEX();
+    }
+}
